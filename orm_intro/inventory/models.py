a710a82aba1ed8386d5587b451b1ba570d711aa1
babadu14 <andriamanagadze7@gmail.com>
2024-11-22 22:33:11 +0400

    __str__ added

--- orm_intro/inventory/models.py
+++ orm_intro/inventory/models.py
@@ -6,3 +6,6 @@ class Inventory(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10,decimal_places=2)
     stock = models.IntegerField()
+    
+    def __str__(self):
+        return f"{self.name} {self.price} {self.stock}"
